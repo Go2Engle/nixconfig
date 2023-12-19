a1dce71de381b1451358825c822dc84dc666ef0b
Chris Engle <go2engle@gmail.com>
2023-12-19 12:15:50 -0500

    Remove yai and commented out code

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -148,7 +148,6 @@
     vlc
     #google-chrome
     brave
-    yai
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -163,12 +162,6 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
