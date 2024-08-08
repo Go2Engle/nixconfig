19ef56dddad3994c399daa7546cda9acc46a231d
Chris Engle <go2engle@gmail.com>
2024-08-08 13:54:17 -0400

    updates

--- hosts/framework/home.nix
+++ hosts/framework/home.nix
@@ -105,7 +105,9 @@
 
   programs.zsh = {
     enable = true;
-    enableAutosuggestions = true;
+    autosuggestions = {
+      enable = true;
+    };
     enableCompletion = true;
     oh-my-zsh = {
       enable = true;
