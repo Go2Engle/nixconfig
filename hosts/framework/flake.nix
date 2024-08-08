34c0dd28c740f260d7c9e49aaed39b0156ce8b3d
Chris Engle <go2engle@gmail.com>
2024-08-08 13:44:06 -0400

    fix

--- hosts/framework/flake.nix
+++ hosts/framework/flake.nix
@@ -1,15 +1,15 @@
-
+{
   description = "framework flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20511";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      "framework" = nixpkgs.lib.niosSystem {
+      "framework" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ 
           ./configuration.nix
